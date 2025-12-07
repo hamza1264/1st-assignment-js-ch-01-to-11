d2ab8d5c5ee116eb61252717875c253f13a22bea
hamza ansari <hamza.aiet.student@gmail.com>
2025-12-07 11:34:47 -0800

    js first assignment

---
+++ index.js
@@ -0,0 +1,238 @@
+// // Assignment for chapter 01 to 10
+
+
+// // ch # 1 : alert questions start
+
+// // Q=1
+// alert("Hello World");
+
+// // Q=2
+// alert("Hamza Ansari");
+
+// // Q=3
+// alert(2);
+
+// // Q=4
+// var massage = "Hello World";
+// alert(massage);
+
+// // Q=5
+// var myName = "Hamza ";
+// var caste = "Ansari";
+// var result = myName + caste
+// alert(result)
+
+// // ch # 1 : alert question complited
+
+
+// // ch # 2 : variable for string questions start
+
+// // Q=1
+// var userName = "Hamza Ansari";
+
+// // Q=2
+// var massage = "Welcome";
+// alert(massage);
+
+// // Q=3
+// var firstName = "Hamza";
+// var lastName = " Ansari";
+// var fullName = firstName + lastName
+// alert(fullName);
+
+// // Q=4
+// var favoriteQuote = "Success is not a luck, it's a game of patience and hard work.";
+// alert(favoriteQuote);
+
+// // Q=5
+// var city = "karachi";
+// alert("Welcome to " + city + " !");
+
+// // ch # 2 : variable for string questions complited
+
+
+// // ch # 3 : variable for number questions start 
+
+// // Q=1
+// var age ="18";
+// // alert(age)
+// console.log(age)
+
+// // Q=2
+// var add1 = 4
+// var add2 = 6
+// var result = add1 + add2
+// console.log(result)
+
+// // Q=3
+// var sub1 = 10
+// var sub2 = 4
+// var result = sub1 - sub2
+// console.log(result)
+
+// // Q=4
+// var mul1 = 2
+// var mul2 = 5
+// var result = mul1 * mul2
+// console.log(result)
+
+// // Q=5
+// var age2 = 20
+// console.log("I am " + age2 + " years old.")
+
+// // ch # 3 : variable for number questions complited
+
+
+// // ch # 4 : maths expressions questions start
+
+// // Q=1
+// var x = 10;
+// var y = 5;
+// var add = x + y;
+// console.log(add)
+
+// // Q=2 
+// var x = 10;
+// var y = 5;
+// var sub = y - x
+// console.log(sub)
+
+// // Q=3
+// var x = 10;
+// var y = 5;
+// var mul = x * y
+// console.log(mul)
+
+// // Q=4
+// var x = 10;
+// var y = 5;
+// var div = x / y
+// console.log(div)
+
+// // Q=5
+// var x = 10;
+// var y = 5;
+// var mod = x % y
+// console.log(mod)
+
+// // ch # 4 : maths expressions questions complited
+
+
+// // ch # 5 : prompt questions start
+
+
+// // Q=1
+// let uname = prompt("Enter your name:");
+// console.log("Hello, " + uname)
+
+// // Q=2
+// let age = prompt("Enter your age:");
+// console.log("Yor are " + age + " years old.")
+
+// // Q=3
+// let num = prompt("Enter your number:");
+// let result = num * 2
+// console.log("The result is: " + result)
+
+// // Q=4
+// let color = prompt("Enter your fravorite color;");
+// console.log("Wow! I like " + color + " too!")
+
+// // Q=5
+// let city = prompt("Enter your city:");
+// console.log("You are from " + city + ".")
+
+// // ch # 5 : prompt questions complited
+
+
+// // ch # 6 : .If Statement and Comparison Operators questions start
+
+// // Q=1
+// let age = prompt("Enter your age:");
+// if (age <= 30) {
+//     console.log("You,re still young man." + age)
+// }
+
+// // Q=2
+// let even = prompt("Enter a number:");
+// if (even % 2 === 0) {
+//     console.log("Even number: " + even)
+// } else {
+//     console.log("Not an even number: " + even)
+// }
+
+// // Q=3
+// let number1 = 10;
+// let number2 = 20;
+// if (number1 > number2) {
+//     console.log(number1 + " is greater then " + number2)
+// } else if (number2 > number1) {
+//     console.log(number2 + " is greater then " + number1)
+// } else {
+//     console.log("Both numbers are equal.")
+// }
+
+// // Q=4
+// let currectPassword = "hamza";
+// let userPassword = prompt("Enter your password, 5 Digit:")
+// if (userPassword === currectPassword) {
+//     alert("Access granted.")
+// } else {
+//     alert("Access denied.")
+// }
+
+// // Q=5
+// // a 
+// let a = 5;
+// let b = "5  ";
+// if (a == b){
+//     console.log(a + " == " + b + ": true")
+// }else {
+//     console.log(a + " == " + b + ": false")
+// }
+
+// // b 
+// if (a === b){
+//     console.log(a + " === " + b + ": true")
+// }else {
+//     console.log(a + " === " + b + ": false")
+// }
+
+// // c  
+// if (a != b){
+//     console.log(a + " != " + b + ": true")
+// }else {
+//     console.log(a + " != " + b + ": false")
+// }
+
+// // d 
+// if (a !== b){
+//     console.log(a + " !== " + b + ": true")
+// }else {
+//     console.log(a + " !== " + b + ": false")
+// }
+
+// // ch # 6 : .If Statement and Comparison Operators questions complited
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
